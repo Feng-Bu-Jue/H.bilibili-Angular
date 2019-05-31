f96ad10fe527e0fbd29c7836343586dbab473a0e
FengBujue <978040259@qq.com>
2019-06-01 06:19:20 +0800

    fix error caused  by file name case in macos

---
+++ src/app/bilibiliApi/models/bilibiliProtocal.ts
@@ -0,0 +1,7 @@
+export class BiliBiliProtocal<T> {
+  public code: number;
+  public message: string;
+  public msg: string;
+  public data: T;
+}
+
---
+++ src/app/bilibiliApi/models/linkDrawResult.ts
@@ -0,0 +1,34 @@
+export class Picture {
+  public img_height: number;
+  public img_size: number;
+  public img_width: number;
+  public img_src: string;
+}
+
+
+export class LinkDrawItem {
+  public already_liked: number;
+  public already_voted: number;
+  public category: string;
+  public doc_id:number;
+  public poster_uid: number;
+  public title: string;
+  public upload_time: number;
+  public pictures: Array<Picture>;
+}
+
+export class LinkDrawUesr {
+  public head_url: string;
+  public name: string;
+  public uid: number;
+}
+
+export class LinkDrawResult {
+  public item: LinkDrawItem;
+  public user: LinkDrawUesr;
+}
+
+export class LinkDrawResultList{
+  public total_count: number;
+  public items: LinkDrawResult[];
+}
